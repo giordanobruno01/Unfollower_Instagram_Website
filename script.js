83a53ae40b02b25045e0e818c7746a89f7335b23
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-07-21 16:06:49 +0100

    half responsive

---
+++ script.js
@@ -0,0 +1,7 @@
+function checkExt(filename){
+
+    if (!filename.match(/\.json$/)){
+        alert('Only JSON files are supported. It is recommended to download from Instagram a .json file.');
+    }
+}
+
