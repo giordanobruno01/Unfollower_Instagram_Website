2dc1213e9d2123ccc4c67c250c17910f5966ca53
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-07-22 16:36:11 +0100

    ready to use but needs improvement

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Unfollower_Instagram_Website
+Steps how to download the followers and following file from Instagram
+1 open website or app
+2 go to your profile and click on 3 lines =
+3 find "Your activity" and click on it
+4 go to the end of the page and click on "Download your information"
+5 request a "download from your account"
+7 choose "Select types of information" as the complete copy takes a longer
+8 find and select "followers and following" from the list
+9 click in "format" and select "JSON"
+9 click in "date range" and select "all time"
+10 save and "submit request"
+
+it might take a few minutes but instagram will send you a link when the file is ready, after that just download the file, extract the both "followers_1.json" and "following.json" and add to the program replacing the existing ones.
+
+ps: the json files are inside the folder after you extract
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var iFollow ;
+var followMe;
 var following = [];
 var followers = [];
 var followingLink = [];
@@ -5,42 +7,78 @@ var followersLink = [];
 var followersLength;
 var followingLength;
 
-function checkExt(filename) {
- 
-  if (!filename.match(/\.json$/)) {
+function checkExt(event) {
+  const file = event.target.files[0];
+
+  if (file.type !== "application/json") {
     alert(
       "Only JSON files are supported. It is recommended to download from Instagram a .json file."
     );
+    return;
   }
+  readJsonFile(file)
+    .then((jsonData) => {
+      if (file["name"] == "followers_1.json") {
+        followMe = jsonData;
+        followersLength = followMe.length;
+        for (let index = 0; index < followersLength; index++) {
+          followers.push(followMe[index].string_list_data[0].value);
+          followersLink.push(followMe[index].string_list_data[0].href);
+        }
+      }
+      if (file["name"] == "following.json") {
+        iFollow = jsonData;
+        followingLength = iFollow.relationships_following.length;
+        for (let index = 0; index < followingLength; index++) {
+          following.push(
+            iFollow.relationships_following[index].string_list_data[0].value
+          );
+
+          followingLink.push(
+            iFollow.relationships_following[index].string_list_data[0].href
+          );
+        }
+      }
+    })
+    .catch((error) => {
+      alert("Error reading" + error.message);
+    }); 
+  // if(following.length!=0 && followers.length !=0){
+  //   compare()
+  // }
 }
 
-function uploadFile() {
-  let file = document.querySelector("#following");
-}
+const readJsonFile = (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (readerevent) => {
+      try {
+        const content = readerevent.target.result;
+        const parsedJSON = JSON.parse(content);
+
+        resolve(parsedJSON);
+      } catch (error) {
+        reject(error);
+      }
+    };
+  });
+};
 
 function extraction() {
-    // var iFollow = document.getElementById("following");
-    // var followMe = document.getElementById("followers");
-    // iFollow = require(iFollow);
-    // followMe = require(followMe);
-    // console.log(iFollow)
 
-  var iFollow = require("./following.json");
-  var followMe = require("./followers_1.json");
-
-  followingLength = iFollow.relationships_following.length;
-
+  // followingLength = iFollow.relationships_following.length;
   followersLength = followMe.length;
 
-  for (let index = 0; index < followingLength; index++) {
-    following.push(
-      iFollow.relationships_following[index].string_list_data[0].value
-    );
+  // for (let index = 0; index < followingLength; index++) {
+  //   following.push(
+  //     iFollow.relationships_following[index].string_list_data[0].value
+  //   );
 
-    followingLink.push(
-      iFollow.relationships_following[index].string_list_data[0].href
-    );
-  }
+  //   followingLink.push(
+  //     iFollow.relationships_following[index].string_list_data[0].href
+  //   );
+  // }
   for (let index = 0; index < followersLength; index++) {
     followers.push(followMe[index].string_list_data[0].value);
     followersLink.push(followMe[index].string_list_data[0].href);
@@ -48,19 +86,27 @@ function extraction() {
 }
 
 function compare() {
-  extraction();
+  // extraction();
   var notFollowingBack = [];
+  var notFollowingBackLink = [];
 
   for (let index = 0; index < followingLength; index++) {
     if (!followers.includes(following[index])) {
-      notFollowingBack.push(following[index] + " - " + followingLink[index]);
+      notFollowingBack.push(following[index]);
+      notFollowingBackLink.push(followingLink[index])
     }
   }
-  console.log(notFollowingBack[0]);
-
-//   document.getElementById("unfollowers-list").innerHTML = "notFollowingBack[0]";
-document.getElementById("unfollowers-list").innerHTML = JSON.stringify(notFollowingBack)
-
+  // for (let index = 0; index < notFollowingBack.length; index++) {
+  //   document.getElementById("unfollowers-list").innerHTML = notFollowingBack[index];
+  //   document.getElementById("unfollowers-list-link").innerHTML = notFollowingBackLink[index];
+    
+  // }
+  var l  = "Click to Unfollow <br>";
+  for (let index = 0; index < notFollowingBack.length; index++) {
+    
+    l = l + ( "<a href="+notFollowingBackLink[index]+" target="+ "_blank> "+notFollowingBack[index]+" </a> <br>");  
+  } 
+  document.getElementById("unfollowers-list").innerHTML =
+    l + "<br>Reload to check again, thanks for testing the app ";
+  
 }
-
-compare()
