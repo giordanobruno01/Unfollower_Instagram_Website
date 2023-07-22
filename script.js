3976a7df2c4fc6641183e2112c3b4da77ba9d318
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-07-22 10:03:24 +0100

    small changes, require does not work in JS

--- script.js
+++ script.js
@@ -1,55 +1,64 @@
-    var following = [] ;
-    var followers = [];
-    var followingLink = [];
-    var followersLink = [];
-    var followersLength;
-    var followingLength;
-    
+var following = [];
+var followers = [];
+var followingLink = [];
+var followersLink = [];
+var followersLength;
+var followingLength;
 
-function checkExt(filename){
-
-    if (!filename.match(/\.json$/)){
-        alert('Only JSON files are supported. It is recommended to download from Instagram a .json file.');
-    }
+function checkExt(filename) {
+  if (!filename.match(/\.json$/)) {
+    alert(
+      "Only JSON files are supported. It is recommended to download from Instagram a .json file."
+    );
+  }
 }
 
-function uploadFile(){
-    let file = document.querySelector('#filea');
+function uploadFile() {
+  let file = document.querySelector("#filea");
 }
 
-function extraction(){
+function extraction() {
+    var iFollow = document.getElementById("following");
+    var followMe = document.getElementById("followers");
+    iFollow = require(iFollow);
+    followMe = require(followMe);
+    console.log(iFollow)
 
-    const v = require('./following.json');
-    const f = require('./followers_1.json');
-    followingLength = v.relationships_following.length;
-    followersLength = f.length;
+//   var iFollow = require("./following.json");
+//   var followMe = require("./followers_1.json");
 
+  followingLength = iFollow.relationships_following.length;
 
-    for (let index = 0; index < followingLength; index++) {
-        following.push(v.relationships_following[index].string_list_data[0].value);
-        
-        followingLink.push(v.relationships_following[index].string_list_data[0].href)
-        
-    }
-    for (let index = 0; index < followersLength; index++) {
-        followers.push(f[index].string_list_data[0].value)
-        followersLink.push(f[index].string_list_data[0].href)  
-    }
+  followersLength = followMe.length;
+
+  for (let index = 0; index < followingLength; index++) {
+    following.push(
+      iFollow.relationships_following[index].string_list_data[0].value
+    );
+
+    followingLink.push(
+      iFollow.relationships_following[index].string_list_data[0].href
+    );
+  }
+  for (let index = 0; index < followersLength; index++) {
+    followers.push(followMe[index].string_list_data[0].value);
+    followersLink.push(followMe[index].string_list_data[0].href);
+  }
 }
 
-function compare(){
-    
-    extraction()
-    var notFollowingBack= []
-    
+function compare() {
+  extraction();
+  var notFollowingBack = [];
 
-    for (let index = 0; index < followingLength; index++) {
-        if(!followers.includes(following[index])){
-            notFollowingBack.push(following[index]+ " - " + followingLink[index]);
-
-        }
+  for (let index = 0; index < followingLength; index++) {
+    if (!followers.includes(following[index])) {
+      notFollowingBack.push(following[index] + " - " + followingLink[index]);
     }
-    const string = JSON.stringify(notFollowingBack, null, 4);
+  }
+  console.log(notFollowingBack[0]);
 
-    document.getElementById("unfollowers-list").innerHTML = string;
+  // const string = JSON.stringify(notFollowingBack, null, 4);
+
+  // document.getElementById("unfollowers-list").innerHTML = "notFollowingBack[0]";
 }
+compare();
