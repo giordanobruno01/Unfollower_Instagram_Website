ec56df5007627c654842885ab98b333a5737947f
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-07-27 10:07:37 +0100

    if the list was even there was an error, fixed the lenth/2 problem

--- script.js
+++ script.js
@@ -3,35 +3,34 @@ var followMe;
 var following = [];
 var followers = [];
 var followingLink = [];
-var followersLink = []; 
+var followersLink = [];
 var followersLength;
 var followingLength;
 
 function checkExt(event) {
   const file = event.target.files[0];
- 
- const fileName1 = document.getElementById("followers");
- const fileName2 = document.getElementById("following");
 
- console.log("type", file.type , fileName1.value,"and", fileName2.value);
- console.log()
+  const fileName1 = document.getElementById("followers");
+  const fileName2 = document.getElementById("following");
+
+  console.log("type", file.type, fileName1.value, "and", fileName2.value);
+  console.log();
   if (file.type !== "application/json") {
     //not working this
     alert(
       "Only JSON files are supported. It is recommended to download from Instagram a .json file."
     );
-    
+
     fileName1.value = "";
     fileName2.value = "";
     return;
-
   } else if (file.name != "followers_1.json" && file.name != "following.json") {
     alert(
       "Only files named as following.json or followers_1.json allowed, check again."
     );
-    if (file.name != "followers_1.json"){
+    if (file.name != "followers_1.json") {
       fileName1.value = "";
-    }else if (file.name != "following.json") {
+    } else if (file.name != "following.json") {
       fileName2.value = "";
     }
 
@@ -65,7 +64,6 @@ function checkExt(event) {
       });
   }
 }
-
 
 const readJsonFile = (file) => {
   return new Promise((resolve, reject) => {
@@ -105,11 +103,9 @@ const readJsonFile = (file) => {
 //   }
 // }
 
-function fileIncorrect(){
-   const fileName1 = document.getElementById("followers");
-   const fileName2 = document.getElementById("following");
-
-  
+function fileIncorrect() {
+  const fileName1 = document.getElementById("followers");
+  const fileName2 = document.getElementById("following");
 }
 
 function compare() {
@@ -140,6 +136,7 @@ function compare() {
     " Instagram users do not follow you back <br><br>";
   unf = "Click the link to Unfollow them <br>";
   for (let index = 0; index < notFollowingBack.length / 2; index++) {
+    console.log("list " + index + " " + notFollowingBack[index]);
     l1 =
       l1 +
       ("<a href=" +
@@ -150,24 +147,30 @@ function compare() {
         " </a> <br>");
   }
   for (
-    let index = notFollowingBack.length / 2;
+    let index = (notFollowingBack.length / 2).toFixed(0);
     index < notFollowingBack.length;
     index++
   ) {
-    if (notFollowingBack.length> 1) {
+    console.log("list " + index + " ", notFollowingBack[index]);
+
+    if (notFollowingBack.length > 1) {
       l2 =
-      l2 +
-      ("<a href=" +
-        notFollowingBackLink[index] +
-        " target=" +
-        "_blank> " +
-        notFollowingBack[index] +
-        " </a> <br>");
-    }else{
-      console.log("hello")
+        l2 +
+        ("<a href=" +
+          notFollowingBackLink[index] +
+          " target=" +
+          "_blank> " +
+          notFollowingBack[index] +
+          " </a> <br>");
     }
-    
   }
+
+  console.log(
+    "notffl lenght ",
+    notFollowingBack.length,
+    "  ",
+    notFollowingBack
+  );
   d = "<br>Reload to check again, thanks for testing the website <br>";
 
   document.getElementById("count").innerHTML = c;
@@ -175,7 +178,6 @@ function compare() {
   document.getElementById("unfollowers-listLeft").innerHTML = l1;
   document.getElementById("unfollowers-listRight").innerHTML = l2;
   document.getElementById("final").innerHTML = d;
-
 
   // document.getElementById("unfollowers-list").innerHTML =
   //   '<button type="button" onClick="window.location.reload()">Reload</button>';
